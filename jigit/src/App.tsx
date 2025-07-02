ef80c847308ee9b793ceaca186ea7e001704842d
Gabo1234567890 <gabonenov@gmail.com>
2025-07-02 14:40:15 +0300

    auth guard made

--- jigit/src/App.tsx
+++ jigit/src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./login/LoginPage";
+import AuthGuard from "./guards/AuthGuard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route element={<AuthGuard />}>
+          <Route path="/" element={<p>Home Page</p>} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
