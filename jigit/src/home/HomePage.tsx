31fa9cd2b7c1f588217d7bde2bfe658aaea459c6
Gabo1234567890 <gabonenov@gmail.com>
2025-07-14 10:38:28 +0300

    home page nav bar pulled out

--- jigit/src/home/HomePage.tsx
+++ jigit/src/home/HomePage.tsx
@@ -3,7 +3,6 @@ import { getRecentReports } from "../services/reports";
 import { useEffect, useState } from "react";
 import Report from "../components/Report";
 import SearchReportsBar from "../components/SearchReportsBar";
-import LogoutButton from "../components/LogoutButton";
 import NavBar from "../components/NavBar";
 
 const HomePage = () => {
@@ -25,23 +24,29 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="main-page-background">
-      <NavBar pageTitle="Home Feed" />
-      <Link to={"/profile"}>Profile</Link>
-      <Link to={"/create-report"}>Create Report</Link>
-      <SearchReportsBar
+    <>
+      <NavBar
+        pageTitle="Home Feed"
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
       />
-      {!searchQuery && (
-        <>
-          <h2>Feed:</h2>
-          {recentReports.map((report) => (
-            <Report report={report} key={report.id} hidden={false} />
-          ))}
-        </>
-      )}
-    </div>
+      <div className="main-page-background">
+        <Link to={"/profile"}>Profile</Link>
+        <Link to={"/create-report"}>Create Report</Link>
+        <SearchReportsBar
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
+        {!searchQuery && (
+          <>
+            <h2>Feed:</h2>
+            {recentReports.map((report) => (
+              <Report report={report} key={report.id} hidden={false} />
+            ))}
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
